b56b14c301d1278e7dbaa8c803aa03672cbe2098
xuxin <943512372@qq.com>
2018-06-23 11:26:33 +0800

    通用部分

--- src/component/layout/index.jsx
+++ src/component/layout/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import NavTop from 'component/nav-top/index.jsx';
+import NavSide from 'component/nav-side/index.jsx';
 import './theme.css'
 
 class Layout extends React.Component{
@@ -8,8 +10,8 @@ class Layout extends React.Component{
     render() {
       return (
         <div id="wrapper">
-            <TopNav/>
-            <SideNav/>
+            <NavTop/>
+            <NavSide/>
             {this.props.children}   
         </div>
       )
